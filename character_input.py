c1173cc001894ee4eaeb2d22aabbabddd36212ae
Timfon <timmyoctoidea@gmail.com>
2023-05-29 18:35:58 -0400

    ehr

---
+++ character_input.py
@@ -0,0 +1,9 @@
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+years_until_hundred = 100 - age
+
+current_year = 2023
+
+year_at_hundred = current_year + years_until_hundred
+
+print(name +  " will turn 100 in " + str(year_at_hundred))
